c3631748b044007985994a3c0660bfff86b6d3b1
Per-Erik Martin <pem@pem.nu>
2021-05-14 18:15:15 +0200

    Fixed some output bugs, and added tests for remove, get, and iteration

--- htabunit.c
+++ htabunit.c
@@ -6,6 +6,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "hashtable.h"
 
 static char *Words[] =
@@ -32,6 +33,7 @@ int
 main()
 {
     int i;
+    char *val;
     hashtable_t h;
 
     h = hashtable_create(10, 0.5, 0.8, hash_string_fast, NULL);
@@ -56,7 +58,26 @@ main()
         fprintf(stderr, "Failed to put key %s\n", Words[i]);
         exit(1);
     }
-    printf("### 11 words in table\n");
+    i += 1;
+    printf("### %d words in table\n", i);
+    print_info(h);
+    putchar('\n');
+
+    for (int j = 0 ; j < i && Words[j] != NULL ; j++)
+    {
+        if (hashtable_rem(h, Words[j], (void **)&val) < 0)
+        {
+            fprintf(stderr, "Failed to remove key %s\n", Words[j]);
+            exit(1);
+        }
+        if (strcmp(Words[j], val) != 0)
+        {
+            fprintf(stderr, "Removed value mismatch: %s != %s\n",
+                    Words[j], val);
+            exit(1);
+        }
+    }
+    printf("### All keys removed\n");
     print_info(h);
     putchar('\n');
 
@@ -83,8 +104,41 @@ main()
         fprintf(stderr, "Failed to put key %s\n", Words[i]);
         exit(1);
     }
+    i += 1;
     printf("### %d words in table\n", i);
     print_info(h);
+    putchar('\n');
+
+    for (int j = 0 ; j < i && Words[j] != NULL ; j++)
+    {
+        if (hashtable_get(h, Words[j], (void **)&val) < 0)
+        {
+            fprintf(stderr, "Failed to get key %s\n", Words[j]);
+            exit(1);
+        }
+    }
+    printf("###  Got all keys\n");
+    putchar('\n');
+
+    int count = 0;
+    const char *key;
+    hashtable_iter_t iter;
+    hashtable_iter_init(h, &iter);
+    while (hashtable_iter_next(h, &iter, &key, (void **)&val))
+    {
+        count += 1;
+        if (strcmp(key, val) != 0)
+        {
+            fprintf(stderr, "Key-val mismatch: %s != %s\n", key, val);
+            exit(1);
+        }
+    }
+    if (i != count)
+    {
+        fprintf(stderr, "Iterator found %d key, expected %d\n", count, i);
+        exit(1);
+    }
+    printf("###  Iterated over all keys\n");
     putchar('\n');
 
     hashtable_clear(h);
