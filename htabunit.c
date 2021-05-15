b0da76b15cc6082ca9688d2e9121ec7a6df2275c
Per-Erik Martin <pem@pem.nu>
2021-05-15 16:38:49 +0200

    Added more unit tests

--- htabunit.c
+++ htabunit.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 #include "hashtable.h"
 
 static char *Words[] =
@@ -29,53 +30,67 @@ print_info(hashtable_t h)
            ((float)count) / size);
 }
 
+static void
+perrex(const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    exit(1);
+}
+
 int
 main()
 {
+    extern char *strdup(char *);
     int i;
     char *val;
     hashtable_t h;
 
+    /*
+    ** Small table, default values, fast, test grow
+    */
     h = hashtable_create(10, 0.5, 0.8, hash_string_fast, NULL);
     if (h == NULL)
-    {
-        fprintf(stderr, "Failed to create hash table\n");
-        exit(1);
-    }
+        perrex("Failed to create hash table\n");
     putchar('\n');
     printf("### New table, FAST hash function, min load 0.5, max load 0.8\n");
     print_info(h);
     for (i = 0 ; i < 8 && Words[i] != NULL ; i++)
         if (hashtable_put(h, Words[i], Words[i], NULL) != hashtable_ret_ok)
-        {
-            fprintf(stderr, "Failed to put key %s\n", Words[i]);
-            exit(1);
-        }
+            perrex("Failed to put key %s\n", Words[i]);
     printf("### %d words in table\n", i);
     print_info(h);
     if (hashtable_put(h, Words[i], Words[i], NULL) != hashtable_ret_ok)
-    {
-        fprintf(stderr, "Failed to put key %s\n", Words[i]);
-        exit(1);
-    }
+        perrex("Failed to put key %s\n", Words[i]);
     i += 1;
     printf("### %d words in table\n", i);
     print_info(h);
     putchar('\n');
 
+    /*
+    ** Don't find non-existing key
+    */
+    if (hashtable_get(h, "xyz", (void **)&val) != hashtable_ret_not_found)
+        perrex("Found xyz:%s in table, shouln't have.\n", val);
+    printf("### Get not found test ok\n" );
+    putchar('\n');
+    if (hashtable_rem(h, "abc", (void **)&val) != hashtable_ret_not_found)
+        perrex("Removed abc:%s in table, shouln't have.\n", val);
+    printf("### Remove not found test ok\n");
+    putchar('\n');
+
+    /*
+    ** Remove them one-by-one
+    */
     for (int j = 0 ; j < i && Words[j] != NULL ; j++)
     {
         if (hashtable_rem(h, Words[j], (void **)&val) != hashtable_ret_ok)
-        {
-            fprintf(stderr, "Failed to remove key %s\n", Words[j]);
-            exit(1);
-        }
+            perrex("Failed to remove key %s\n", Words[j]);
         if (strcmp(Words[j], val) != 0)
-        {
-            fprintf(stderr, "Removed value mismatch: %s != %s\n",
-                    Words[j], val);
-            exit(1);
-        }
+            perrex("Removed value mismatch: %s != %s\n", Words[j], val);
     }
     printf("### All keys removed\n");
     print_info(h);
@@ -83,43 +98,40 @@ main()
 
     hashtable_destroy(h);
 
+    /*
+    ** Small table, custom values, "good", test grow
+    */
     h = hashtable_create(10, 0.2, 0.9, hash_string_good, NULL);
     if (h == NULL)
-    {
-        fprintf(stderr, "Failed to create hash table\n");
-        exit(1);
-    }
+        perrex("Failed to create hash table\n");
     printf("### New table, GOOD hash function, min load 0.2, max load 0.9\n");
     print_info(h);
     for (i = 0 ; i < 9 && Words[i] != NULL ; i++)
         if (hashtable_put(h, Words[i], Words[i], NULL) != hashtable_ret_ok)
-        {
-            fprintf(stderr, "Failed to put key %s\n", Words[i]);
-            exit(1);
-        }
+            perrex("Failed to put key %s\n", Words[i]);
     printf("### %d words in table\n", i);
     print_info(h);
     if (hashtable_put(h, Words[i], Words[i], NULL) != hashtable_ret_ok)
-    {
-        fprintf(stderr, "Failed to put key %s\n", Words[i]);
-        exit(1);
-    }
+        perrex("Failed to put key %s\n", Words[i]);
     i += 1;
     printf("### %d words in table\n", i);
     print_info(h);
     putchar('\n');
 
+    /*
+    ** Find them all
+    */
     for (int j = 0 ; j < i && Words[j] != NULL ; j++)
     {
         if (hashtable_get(h, Words[j], (void **)&val) != hashtable_ret_ok)
-        {
-            fprintf(stderr, "Failed to get key %s\n", Words[j]);
-            exit(1);
-        }
+            perrex("Failed to get key %s\n", Words[j]);
     }
-    printf("###  Got all keys\n");
+    printf("### Found all keys\n");
     putchar('\n');
 
+    /*
+    ** Iterate
+    */
     int count = 0;
     const char *key;
     hashtable_iter_t iter;
@@ -128,21 +140,66 @@ main()
     {
         count += 1;
         if (strcmp(key, val) != 0)
-        {
-            fprintf(stderr, "Key-val mismatch: %s != %s\n", key, val);
-            exit(1);
-        }
+            perrex("Key-val mismatch: %s != %s\n", key, val);
     }
     if (i != count)
-    {
-        fprintf(stderr, "Iterator found %d key, expected %d\n", count, i);
-        exit(1);
-    }
-    printf("###  Iterated over all keys\n");
+        perrex("Iterator found %d keys, expected %d\n", count, i);
+    printf("### Iterated over all keys\n");
     putchar('\n');
 
+    /*
+    ** Clear the table
+    */
     hashtable_clear(h);
     printf("### Cleared table\n");
+    print_info(h);
+    putchar('\n');
+
+    hashtable_destroy(h);
+
+    /*
+    ** Default table with deallocator
+    */
+    h = hashtable_create_dest_default(free);
+    if (h == NULL)
+        perrex("Failed to create hash table\n");
+    printf("### New table, default with deallocator\n");
+    print_info(h);
+    for (i = 0 ; Words[i] != NULL ; i++)
+        if (hashtable_put(h, Words[i], strdup(Words[i]), NULL) != hashtable_ret_ok)
+            perrex("Failed to put key %s\n", Words[i]);
+    printf("### %d words in table\n", i);
+    print_info(h);
+    putchar('\n');
+
+    /*
+    ** Replace a value
+    */
+    if (hashtable_put(h, Words[0], strdup("123456"), (void **)&val) != hashtable_ret_replaced)
+        perrex("Failed to replace value for %s\n", Words[0]);
+    if (strcmp(val, Words[0]) != 0)
+        perrex("Old value didn't match: %s != %s\n", val, Words[0]);
+    if (hashtable_get(h, Words[0], (void **)&val) != hashtable_ret_ok)
+        perrex("Failed to get %s\n", Words[0]);
+    if (strcmp(val, "123456") != 0)
+        perrex("New value does not match 123456: %s\n", val);
+    /* Replace again (old value should be freed) */
+    if (hashtable_put(h, Words[0], strdup("ABCDEF"), NULL) != hashtable_ret_replaced)
+        perrex("Failed to replace value for %s\n", Words[0]);
+    printf("### Replace ok\n");
+    print_info(h);
+    putchar('\n');
+
+    /*
+    ** Remove them one-by-one without returning the value.
+    ** (They should be freed.)
+    */
+    for (i = 0 ; Words[i] != NULL ; i++)
+    {
+        if (hashtable_rem(h, Words[i], NULL) != hashtable_ret_ok)
+            perrex("Failed to remove key %s\n", Words[i]);
+    }
+    printf("### All keys removed\n");
     print_info(h);
     putchar('\n');
 
